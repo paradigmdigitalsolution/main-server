5188e62b88cfc887c23ffa537d5db198fd039092
paradigmdigitalsolution <paradigmdigitalsolution@gmail.com>
2025-05-06 23:29:31 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ async function connectToDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       bufferCommands: false,
-      serverSelectionTimeoutMS: 60000,
+      serverSelectionTimeoutMS: 2000000,
     });
     isConnected = true;
     console.log("✅ MongoDB Connected");
