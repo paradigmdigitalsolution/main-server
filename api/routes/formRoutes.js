9663a6f0c860da9d37056d440e899650f65a8582
paradigmdigitalsolution <paradigmdigitalsolution@gmail.com>
2025-05-07 00:01:19 +0500

    dgdf

--- api/routes/formRoutes.js
+++ api/routes/formRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { formSubmission } from "../controller/formSubmissionController.js"
 import { connectToDB } from "../db.js";
+import FormModel from "../model/formSubmission.js";
 const router = Router()
 
 router.post("/submit", async (req, res) => {
